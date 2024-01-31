c73f0a89d1356bc2d700b6782c9a5cc9679b4221
Harshal A <harshal73gh@gmail.com>
2024-01-31 22:17:07 +0530

    nav fixed

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -3,19 +3,20 @@ import React from 'react'
 const Navbar
  = () => {
   return (
-    <div className='flex items-center bg-black text-white  '>Asker
+    <div className=' px-20 flex items-center bg-black text-white  '>
     <div >
         
             <div className='flex' >
-            <ul className='flex flex-row mt-0 space-x-12 rtl:space-x-reverse text-sm ' >
-                <li className='pl-10'  >Forums</li>
-                <li >Community Standards</li>
-                <li>All Activity</li>
-                <li>More</li>
-                <li >Exsisting User?,Sign In</li>
-                <li>Sign Up</li>
+            <ul className='flex flex-row  mt-3 space-x-20 rtl:space-x-reverse text-sm ' >
+            <li className='pl-10 font-signature text-2xl '  >Asker</li>
+                <li className='pl-10 text-gray-200  hover:text-orange-500 cursor-pointer'  >Forums</li>
+                <li className='text-gray-200 hover:text-orange-500 cursor-pointer' >Community Standards</li>
+                <li className='text-gray-200 hover:text-orange-500 cursor-pointer'>All Activity</li>
+                <li className='text-gray-200 hover:text-orange-500 cursor-pointer'>More</li>
+                <li className='pl-30 text-gray-200 hover:text-orange-500  cursor-pointer' >Exsisting User?,Sign In</li>
+                <li  className='pl-15 text-gray-200  hover:text-orange-500  cursor-pointer'>Sign Up</li>
             </ul>
-            </div>
+            </div> 
             
             </div>
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily:{
+    signature:['Sixtyfour']
+    },
   },
   plugins: [],
 }
